5f5ab0ba42c2c592d2a3e6fb97b9f65d2048b10e
arielkeybob <arielkeybob@gmail.com>
2023-11-09 20:01:33 -0300

    User professional agora só ve seus próprios services na lista

--- includes/class-professionaldirectory-admin.php
+++ includes/class-professionaldirectory-admin.php
@@ -5,23 +5,40 @@ if (!defined('WPINC')) {
 }
 
 class ProfessionalDirectory_Admin {
-    public static function init() {
-        add_action('pre_get_posts', [self::class, 'restrict_service_view']);
+    // Construtor da classe
+    public function __construct() {
+        add_action('pre_get_posts', [$this, 'restrict_service_view']);
+        add_filter('views_edit-professional_service', [$this, 'adjust_service_views']);
     }
 
-    public static function restrict_service_view($query) {
-        if (is_admin() && $query->is_main_query() && $query->get('post_type') === 'service') {
-            // Verifique se a função existe para evitar erros em uma instalação limpa do WordPress.
-            if (function_exists('get_current_user_id') && !current_user_can('administrator')) {
-                $user_id = get_current_user_id();
-                // Ajuste a verificação do papel se necessário.
-                if ($user = get_userdata($user_id) && in_array('professional', (array) $user->roles)) {
-                    $query->set('author', $user_id);
-                }
+    // Função para restringir a visualização de serviços no admin
+    public function restrict_service_view($query) {
+        if (is_admin() && $query->is_main_query() && $query->get('post_type') === 'professional_service') {
+            if (current_user_can('professional') && !current_user_can('administrator')) {
+                $query->set('author', get_current_user_id());
             }
         }
     }
+
+    // Função para ajustar as abas na tela de listagem de serviços
+    public function adjust_service_views($views) {
+        if (get_post_type() === 'professional_service' && current_user_can('professional') && !current_user_can('administrator')) {
+            // Remover a aba "All"
+            if (isset($views['all'])) {
+                unset($views['all']);
+            }
+
+            // Definir a aba "Mine" como a aba padrão ativa
+            if (isset($views['mine'])) {
+                $views['mine'] = str_replace('class="', 'class="current ', $views['mine']);
+                // Ajustar o link para não filtrar por status
+                $views['mine'] = preg_replace('/<a(.*)>(.*)<\/a>/', '<a$1 aria-current="page">$2</a>', $views['mine']);
+            }
+        }
+
+        return $views;
+    }
 }
 
-// Inicializa a classe de administração.
-ProfessionalDirectory_Admin::init();
+// Instanciar a classe
+new ProfessionalDirectory_Admin();
