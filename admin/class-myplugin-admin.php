cd022e7f99cb9fccd9ceea135309da8a04b37ff3
arielkeybob <arielkeybob@gmail.com>
2023-11-25 01:07:58 -0300

    Adicionado função para enviar cópia do email para admin ou outro email

--- admin/class-myplugin-admin.php
+++ admin/class-myplugin-admin.php
@@ -35,7 +35,10 @@ class MyPlugin_Admin {
 
     public function register_settings() {
         register_setting('myplugin_settings_group', 'myplugin_google_maps_api_key');
+        register_setting('myplugin_settings_group', 'myplugin_selected_admins');
+        register_setting('myplugin_settings_group', 'myplugin_manual_emails');
         
+        // Seção de Configurações da API
         add_settings_section(
             'myplugin_api_settings_section',
             'API Settings',
@@ -50,11 +53,56 @@ class MyPlugin_Admin {
             'myplugin',
             'myplugin_api_settings_section'
         );
+    
+        // Nova Seção de Configurações de E-mail
+        add_settings_section(
+            'myplugin_email_settings_section',
+            'Configurações de E-mail',
+            null,
+            'myplugin'
+        );
+    
+        add_settings_field(
+            'myplugin_selected_admins',
+            'Admins para Receber E-mails',
+            array($this, 'selected_admins_callback'),
+            'myplugin',
+            'myplugin_email_settings_section'
+        );
+    
+        add_settings_field(
+            'myplugin_manual_emails',
+            'E-mails Adicionais',
+            array($this, 'manual_emails_callback'),
+            'myplugin',
+            'myplugin_email_settings_section'
+        );
     }
+    
     
     public function google_maps_api_key_callback() {
         $api_key = get_option('myplugin_google_maps_api_key');
         echo "<input type='text' name='myplugin_google_maps_api_key' value='" . esc_attr($api_key) . "' />";
+    }
+
+    public function selected_admins_callback() {
+        $selected_admins = get_option('myplugin_selected_admins', []);
+        $admins = get_users(['role' => 'administrator']);
+    
+        echo '<select multiple name="myplugin_selected_admins[]" style="width: 100%;">';
+        foreach ($admins as $admin) {
+            $selected = in_array($admin->user_email, $selected_admins) ? 'selected' : '';
+            $admin_display = sprintf('%s (%s)', $admin->display_name, $admin->user_email);
+            echo '<option value="' . esc_attr($admin->user_email) . '" ' . $selected . '>' . esc_html($admin_display) . '</option>';
+        }
+        echo '</select>';
+    }
+    
+
+    public function manual_emails_callback() {
+        $manual_emails = get_option('myplugin_manual_emails', '');
+        echo "<input type='text' name='myplugin_manual_emails' value='" . esc_attr($manual_emails) . "' style='width: 50%;' placeholder='email1@example.com, email2@example.com' />";
+        echo "<p>Insira os e-mails adicionais separados por vírgulas.</p>";
     }
     
 }
--- includes/email-functions.php
+++ includes/email-functions.php
@@ -7,6 +7,19 @@ if ( ! defined( 'WPINC' ) ) {
 function send_email_to_service_author($post_id, $user_data) {
     $author_email = get_the_author_meta('user_email', get_post_field('post_author', $post_id));
 
+    // Recupera os e-mails dos administradores selecionados e e-mails adicionais
+    $selected_admins = get_option('myplugin_selected_admins', []);
+    $manual_emails = explode(',', get_option('myplugin_manual_emails', ''));
+
+    // Prepara os endereços de e-mail para BCC
+    $bcc_emails = array_merge($selected_admins, $manual_emails);
+    $bcc_emails = array_filter(array_map('trim', $bcc_emails)); // Remove espaços extras e e-mails vazios
+
+    $headers = [];
+    foreach ($bcc_emails as $bcc_email) {
+        $headers[] = 'Bcc: ' . sanitize_email($bcc_email);
+    }
+
     $name = sanitize_text_field($user_data['name']);
     $email = sanitize_email($user_data['email']);
     $service_type = sanitize_text_field($user_data['service_type']);
@@ -15,5 +28,6 @@ function send_email_to_service_author($post_id, $user_data) {
     $subject = "Consulta de Serviço: " . get_the_title($post_id);
     $message = "Nome: $name\nEmail: $email\nTipo de Serviço: $service_type\nEndereço: $address\n\nServiço Encontrado: " . get_the_title($post_id) . "\nID do Post: $post_id";
 
-    wp_mail($author_email, $subject, $message);
+    wp_mail($author_email, $subject, $message, $headers);
 }
+
