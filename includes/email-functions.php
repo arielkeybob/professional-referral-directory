1a3299bcf2e35e7a1b4294b8db8e7cee080cd551
arielkeybob <arielkeybob@gmail.com>
2024-02-04 14:36:38 -0300

    Bugfix: Separando as preferencias de email do autor e dos admins (#6)

--- includes/email-functions.php
+++ includes/email-functions.php
@@ -14,44 +14,56 @@ function send_email_to_service_author($post_id) {
     // Verifica a preferência de e-mail do profissional
     $email_preference = get_post_meta($post_id, '_pdr_email_preference', true);
 
-    // Se o profissional optou por não receber e-mails, retorna sem enviar
-    if ($email_preference != '1') {
-        return; // Encerra a função se o profissional optou por não receber e-mails
+        // Se o profissional optou por receber e-mails, continua com a lógica de envio
+    if ($email_preference == '1') {
+        // Obtém o e-mail do autor do post
+        $author_email = get_the_author_meta('user_email', get_post_field('post_author', $post_id));
+
+        // Prepara os dados do e-mail
+        $service_title = get_the_title($post_id);
+        $service_url = get_permalink($post_id);
+        
+        // Assunto do e-mail
+        $subject = sprintf('Notificação de Visualização: %s', $service_title);
+        
+        // Mensagem do e-mail
+        $message = sprintf(
+            "Olá,\n\nSeu serviço, '%s', foi recentemente visualizado no nosso site.\nVocê pode vê-lo aqui: %s\n\nAtenciosamente,\nEquipe",
+            $service_title,
+            $service_url
+        );
+        
+        // Cabeçalhos de e-mail (opcional)
+        $headers = array('Content-Type: text/plain; charset=UTF-8');
+        
+        // Envia o e-mail para o autor do serviço
+        wp_mail($author_email, $subject, $message, $headers);
     }
 
-    // Continua com a lógica de envio de e-mail se o profissional optou por receber e-mails
 
-    // Obtém o e-mail do autor do post
-    $author_email = get_the_author_meta('user_email', get_post_field('post_author', $post_id));
+    // Independente da preferência do autor, envia e-mails para administradores/e-mails adicionais
+    send_admin_notification_emails($post_id);
+}
 
+function send_admin_notification_emails($post_id) {
     // Recupera e-mails adicionais das configurações do plugin
     $selected_admins = get_option('myplugin_selected_admins', []);
     $manual_emails = explode(',', get_option('myplugin_manual_emails', ''));
 
     // Prepara endereços de e-mail para BCC
     $bcc_emails = array_merge($selected_admins, $manual_emails);
-    $bcc_emails = array_filter(array_map('trim', $bcc_emails));
+    $bcc_emails = array_filter(array_map('trim', $bcc_emails)); // Limpa e remove vazios
 
     // Prepara os cabeçalhos de e-mail
-    $headers = array_map(function($email) { return 'Bcc: ' . sanitize_email($email); }, $bcc_emails);
+    $headers = [];
+    foreach ($bcc_emails as $email) {
+        $headers[] = 'Bcc: ' . sanitize_email($email);
+    }
 
-    // Prepara os dados do e-mail
-    $name = sanitize_text_field($user_data['name']);
-    $email = sanitize_email($user_data['email']);
-    $service_type = sanitize_text_field($user_data['service_type']);
-    $service_location = sanitize_text_field($user_data['service_location']);
+    // Prepara os dados do e-mail (Exemplo simples, ajuste conforme necessário)
+    $subject = "Notificação do Serviço: " . get_the_title($post_id);
+    $message = "Um serviço foi visualizado: " . get_permalink($post_id);
 
-    // Prepara o assunto e a mensagem
-    $subject = __("Service Inquiry:", "professional_directory") . " " . get_the_title($post_id);
-    $message = sprintf(
-        __("Name: %s\nEmail: %s\nService Type: %s\nLocation: %s\n\nService Found: %s\nPost ID: %s", "professionaldirectory"),
-        $name, $email, $service_type, $service_location, get_the_title($post_id), $post_id
-    );
-    
     // Envia o e-mail
-    wp_mail($author_email, $subject, $message, $headers);
-
-    error_log('Enviando email para o autor do post. Post ID: ' . $post_id);
-    error_log('Dados do usuário: ' . print_r($user_data, true));
+    wp_mail(null, $subject, $message, $headers);
 }
-
