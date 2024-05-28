5a5947222bd6f2f3f4e34c3279af3c2585ab6c8c
arielkeybob <arielkeybob@gmail.com>
2024-05-28 13:44:09 -0300

    Incluindo opcoes do plugin da tela confiruracao no script de exclusao

--- uninstall.php
+++ uninstall.php
@@ -22,13 +22,31 @@ if ($delete_data === 'yes') {
     }
 
     // Deleta as opções do plugin
+    // Deleta as opções do plugin
     $options_to_delete = [
         'pdr_version',
         'pdr_delete_data_on_uninstall',
         'pdr_commission_type',
         'pdr_general_commission_view',
-        'pdr_general_commission_approval'
-        // Adicione outras opções que o plugin cria, se houver.
+        'pdr_general_commission_approval',
+        'pdr_google_maps_api_key',
+        'pdr_selected_admins',
+        'pdr_manual_emails',
+        'pdr_button_color',
+        'pdr_button_text_color',
+        'pdr_button_hover_color',
+        'pdr_button_text_hover_color',
+        'pdr_title_font_family',
+        'pdr_title_color',
+        'pdr_body_font_family',
+        'pdr_body_color',
+        'pdr_template_choice',
+        'pdr_primary_color',
+        'pdr_secondary_color',
+        'pdr_text_color',
+        'pdr_accent_color',
+        'pdr_panel_logo',
+        'pdr_export_data'
     ];
 
     foreach ($options_to_delete as $option) {
