e432cd63a7ec5430fd0e3c8ae9f622dbf0e7a380
arielkeybob <arielkeybob@gmail.com>
2024-06-12 16:21:28 -0300

    Adicionado link aos notices

--- panel/panel-notifications.php
+++ panel/panel-notifications.php
@@ -16,30 +16,22 @@ function rhb_exibir_notificacoes_admin() {
     if (!current_user_can('manage_options')) {
         return;
     }
-    $screen = get_current_screen();
-    if ($pagenow == 'edit.php' && isset($_GET['post_type']) && $_GET['post_type'] == 'rhb_service') {
-        if ($_GET['page'] != 'rhb-general-settings' && $_GET['page'] != 'dashboard-admin') {
-            return;
-        }
-    } else {
-        return;
-    }
 
     $notificacoes = rhb_obter_notificacoes_ativas();
-echo '<div id="rhb-notifications-container">';
-echo '<div class="rhb-notification-slider">';
-foreach ($notificacoes as $notification) {
-    if (!isset($_SESSION['rhb_notification_fechada_' . $notification['id']])) {
-        $imagem_url = plugins_url('img/' . $notification['imagem'], __FILE__);  // Constrói o caminho absoluto
-        echo "<div class='notice notice-info is-dismissible rhb-notification' data-notification-id='{$notification['id']}'>
-                <img src='{$imagem_url}' class='rhb-notification-icon' alt='Notification Icon'>
-                <div class='rhb-notification-content'>
-                    <p><strong>{$notification['titulo']}</strong></p>
-                    <p>{$notification['mensagem']}</p>
-                </div>
-              </div>";
+    echo '<div id="rhb-notifications-container">';
+    echo '<div class="rhb-notification-slider">';
+    foreach ($notificacoes as $notification) {
+        if (!isset($_SESSION['rhb_notification_fechada_' . $notification['id']])) {
+            $imagem_url = plugins_url('img/' . $notification['imagem'], __FILE__);  // Constrói o caminho absoluto
+            echo "<div class='notice notice-info is-dismissible rhb-notification' data-notification-id='{$notification['id']}'>
+                    <img src='{$imagem_url}' class='rhb-notification-icon' alt='Notification Icon'>
+                    <div class='rhb-notification-content'>
+                        <p><strong>{$notification['titulo']}</strong></p>
+                        <p>{$notification['mensagem']}</p>
+                    </div>
+                  </div>";
+        }
     }
-}
 echo '</div><div class="rhb-notification-controls"><button class="prev">Prev</button><button class="next">Next</button></div>';
 echo '</div>';
 }
