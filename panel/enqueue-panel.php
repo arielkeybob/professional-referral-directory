f60f0c5caf6d162c8e23e2940d2fecb8abfd3995
arielkeybob <arielkeybob@gmail.com>
2024-06-14 13:50:20 -0300

    Garantindo que o script e style das notificações estejam disponíveis em todo contexto do plugin.

--- panel/enqueue-panel.php
+++ panel/enqueue-panel.php
@@ -1,26 +1,27 @@
 <?php
 defined('ABSPATH') or die('No script kiddies please!');
 
-// Função para enfileirar estilos específicos
 function referralhub_enqueue_admin_styles($hook_suffix) {
-    if (strpos($hook_suffix, 'rhb') !== false) {
+    $screen = get_current_screen();
+    if (strpos($hook_suffix, 'rhb') !== false || ($screen->post_type === 'rhb_service') || in_array($screen->taxonomy, ['service_type', 'service_location'])) {
         wp_enqueue_style('referralhub-admin-style', plugins_url('/panel/css/admin-style.css', RHB_MAIN_FILE));
         wp_enqueue_style('rhb-dashboard-admin-style', plugins_url('/panel/css/dashboard-style-admin.css', RHB_MAIN_FILE));
         wp_enqueue_style('datatables-css', 'https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css');
+        wp_enqueue_style('rhb-admin-notifications-style', plugins_url('/panel/css/admin-notifications.css', RHB_MAIN_FILE));
     }
 }
 
 // Função para enfileirar scripts específicos
 function referralhub_enqueue_admin_scripts($hook_suffix) {
-    if (strpos($hook_suffix, 'rhb') !== false) {
+    $screen = get_current_screen();
+    if (strpos($hook_suffix, 'rhb') !== false || ($screen->post_type === 'rhb_service') || in_array($screen->taxonomy, ['service_type', 'service_location'])) {
         wp_enqueue_script('dashboard-script-admin', plugins_url('/panel/js/dashboard-script-admin.js', RHB_MAIN_FILE), array('jquery'), null, true);
-        wp_enqueue_script('rhb-admin-notifications', plugins_url('/panel/js/admin-notifications.js', RHB_MAIN_FILE), array('jquery'), null, true);
+        wp_enqueue_script('rhb-admin-notifications-script', plugins_url('/panel/js/admin-notifications.js', RHB_MAIN_FILE), array('jquery'), null, true);
         wp_enqueue_script('rhb-admin-script', plugins_url('/panel/js/admin-script.js', RHB_MAIN_FILE), array('jquery'), null, true);
         wp_enqueue_script('rhb-panel-ajax-script', plugins_url('/panel/js/rhb-panel-ajax.js', RHB_MAIN_FILE), array('jquery'), null, true);
         wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', array('jquery'), null, true);
         wp_enqueue_script('datatables-js', 'https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js', array('jquery'), null, true);
         wp_enqueue_script('chart-js', 'https://cdn.jsdelivr.net/npm/chart.js', array('jquery'), null, true);
-        wp_enqueue_script('rhb-settings-page-script', plugins_url('panel/js/settings-page-colors.js', RHB_MAIN_FILE));
         
         wp_localize_script('rhb-panel-ajax-script', 'rhbPanelAjax', [
             'ajax_url' => admin_url('admin-ajax.php'),
